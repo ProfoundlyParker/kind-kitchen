4bef49b288ab3e5e5f50852d1861d9c9aaf4ec86
ProfoundlyParker <parkerramey@yahoo.com>
2024-05-03 17:14:13 -0400

    Adding Header component comments

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,13 +1,14 @@
 <script>
-
+    import {openModal} from '../store'
 </script>
 
+<!-- Displays brand Header and navigation menu button -->
 <header class="flex flex-col relative z-20">
     <div class="max-w-[1400px] mx-auto w-full flex items-center justify-between p-4 py-6">
         <a href="/">
-            <h1 class="font-semibold">Kind <span class="text-green-400">Kitchen</span></h1>
+            <h1 class="font-semibold"><span class="text-green-400">Kind</span> Kitchen</h1>
         </a>
-        <button class="md:hidden grid place-items-center">
+        <button on:click={() => $openModal = true} class="md:hidden grid place-items-center">
             <i class="fa-solid fa-bars"></i> 
         </button>
         <nav class="hidden md:flex items-center gap-4 lg:gap-6">
