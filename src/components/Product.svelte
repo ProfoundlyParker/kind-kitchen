fdf7e1385f0a3680d78bee4b99620c77b89be65d
ProfoundlyParker <parkerramey@yahoo.com>
2024-05-03 17:16:03 -0400

    Adding Product + ProductCard component comments

--- src/components/Product.svelte
+++ src/components/Product.svelte
@@ -1,6 +1,8 @@
 <script>
   import ProductCard from "./ProductCard.svelte";
-import SectionWrapper from "./SectionWrapper.svelte";
+  import SectionWrapper from "./SectionWrapper.svelte";
+
+  // Product Features
   let productFeatures = [
     {
         featureList: [
@@ -33,9 +35,9 @@ import SectionWrapper from "./SectionWrapper.svelte";
         imgUrl: "assets/toast.jpg",
     }
   ]
-
 </script>
 
+<!-- Displays Product section and ProductCards and info/images -->
 <SectionWrapper id="product">
     <div class="flex flex-col gap-10 sm:gap-14 md:gap-24 flex-1 items-center justify-center pb-10 md:pb-14">
         <div class="flex flex-col gap-2">
