f4306f24264cfd6e54ee514a5ce2cf9c77d8c4e4
ProfoundlyParker <parkerramey@yahoo.com>
2024-05-07 17:24:30 -0400

    Reduced FAQs padding for better continuity

--- src/components/FAQs.svelte
+++ src/components/FAQs.svelte
@@ -55,7 +55,7 @@
 
 <!-- FAQ Section -->
 <SectionWrapper id="faqs">
-    <div class="flex flex-col gap-10 sm:gap-14 md:gap-24 py-20 flex-1 items-center justify-center">
+    <div class="flex flex-col gap-10 sm:gap-14 md:gap-24 pt-10 pb-20 flex-1 items-center justify-center">
         <div class="flex flex-col gap-2">
             <p class="opacity-60 text-base sm:text-lg md:text-xl text-center">
                 For all your kind-eating questions
