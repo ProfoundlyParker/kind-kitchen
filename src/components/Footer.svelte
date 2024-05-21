009e4b22f7d726ef35461c97ff8536f3430f66f0
ProfoundlyParker <parkerramey@yahoo.com>
2024-05-21 17:19:49 -0400

    Increased h1 size and added research href link

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -2,7 +2,7 @@
 <footer class="py-16 sm:py-20 md:py-24 px-4 md:px-8">
     <div class="max-w-[1200px] mx-auto w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-8 text-base">
         <div class="flex flex-col gap-4 md:col-span-2">
-            <h1 class="font-semibold">
+            <h1 class="font-semibold text-2xl">
                 <span class="text-green-400">Kind</span> Kitchen
             </h1>
             <p>©2024 KIND KITCHEN LLC. All rights reserved.</p>
@@ -13,7 +13,7 @@
         </div>
         <div class="flex flex-col gap-4">
             <p class="font-bold poppins text-base sm:text-lg">Research</p>
-            <a href="/" target="_blank" class="cursor-pointer hover:text-green-400 duration-200">Read the science</a>
+            <a href="https://thehumaneleague.org/article/environmental-benefits-of-veganism" target="_blank" class="cursor-pointer hover:text-green-400 duration-200">Benefits of Eating Kind</a>
         </div>
         <div class="flex flex-col gap-4">
             <p class="font-bold poppins text-base sm:text-lg">Follow Us</p>
