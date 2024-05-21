9f46d09fd699e2c1be9578b333ddd45716e35e45
ProfoundlyParker <parkerramey@yahoo.com>
2024-05-21 17:18:17 -0400

    Changed Conversion text for vegan restaurant

--- src/components/Conversion.svelte
+++ src/components/Conversion.svelte
@@ -5,9 +5,9 @@
 
 <!-- Displays CTA buttons with dark stylings -->
 <section class="py-14 md:py-20 flex flex-col gap-8 bg-[#181b34] text-white text-center items-center justify-center">
-    <h4 class="text-xl sm:text-2xl md:text-3xl poppins">Reach your kind eating <span class="poppins font-bold">goals and aspirations</span></h4>
+    <h4 class="text-xl sm:text-2xl md:text-3xl poppins">Try our <span class="poppins font-bold">kind and vegan food</span> today!</h4>
     <p class="poppins text-base sm:text-lg md:text-xl">
-        ✦ Unlimited All You Can Eat ✦
+        🌱 Vegan, fair, and sustainable 🌱
     </p>
     <CtAs dark={true}/>
 </section>
