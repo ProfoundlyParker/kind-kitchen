5329984ee720aaebada5ccc17be811add2480f06
ProfoundlyParker <parkerramey@yahoo.com>
2024-05-22 16:01:50 -0400

    Changed MenuCard view and centered cards

--- src/components/MenuCard.svelte
+++ src/components/MenuCard.svelte
@@ -5,11 +5,11 @@
 </script>
 
 <!-- Displays Menu info and images -->
-<div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 lg:gap-0.5 pb-6">
-    <div class="flex flex-col gap-2">
-            <h4 class="text-lg font-semibold">{menuFeature.name}</h4>
-            <p class="text-green-500 font-semibold">{menuFeature.price}</p>
-            <p class="text-sm text-gray-600">{menuFeature.description}</p>
-    </div>
-    <img src={menuFeature.imgUrl} alt="menu-item" class="rounded-lg shadow-md dropShadow menu-image" style="margin-left: 7px;">
+<div class="flex flex-col sm:flex-row text-left px-4 gap-8 lg:gap-0.5 pb-6">
+  <div class="flex flex-col gap-2 w-full sm:w-1/2">
+      <h4 class="text-lg font-semibold">{menuFeature.name}</h4>
+      <p class="text-green-500 font-semibold">{menuFeature.price}</p>
+      <p class="text-sm text-gray-600 px-0.5">{menuFeature.description}</p>
+  </div>
+  <img src={menuFeature.imgUrl} alt="menu-item" class="rounded-lg shadow-md sm:w-1/2 object-cover h-40 sm:h-48 lg:h-56 dropShadow menu-image">
 </div>
